fdb3171f4f68d43e779db462a9e3028662933bda
blue997 <blue.wave.surf.ocean@gmail.ccom>
2024-11-25 16:24:03 +0900

    parcel bundler project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// node.js 방식 -> common.js
+const autoprefixer=require('autoprefixer');
+module.exports={
+  plugins:[
+    autoprefixer
+  ]
+};
+
+// // ES
+// import autoprefixer from 'autoprefixer';
+// export {
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
